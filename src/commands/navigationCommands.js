95619d4518a5e6d4b28603f2ab86360388a95bac
Natalia Efimova <n.efimova@systtech.ru>
2025-05-04 17:34:35 +0500

    feat: implement navigation commands module

---
+++ src/commands/navigationCommands.js
@@ -0,0 +1,49 @@
+import { chdir, cwd } from 'process';
+import { resolve, dirname } from 'path';
+import { mkdir } from 'fs/promises';
+import * as logger from '../utils/logger.js';
+
+export function up() {
+    const currentDir = cwd();
+    const parentDir = dirname(currentDir);
+    if (currentDir !== parentDir && currentDir !== resolve('/')) {
+        chdir(parentDir);
+    }
+}
+
+export async function changeDirectory(path) {
+    try {
+        const newPath = resolve(cwd(), path);
+        chdir(newPath);
+    } catch {
+        logger.printOperationError();
+    }
+}
+
+export async function listDirectory() {
+    try {
+        const { readdir } = await import('fs/promises');
+        const files = await readdir(cwd(), { withFileTypes: true });
+
+        const directories = files.filter(file => file.isDirectory());
+        const regularFiles = files.filter(file => !file.isDirectory());
+
+        const tableData = [
+            ...directories.map(dir => ({ Name: dir.name, Type: 'directory' })),
+            ...regularFiles.map(file => ({ Name: file.name, Type: 'file' }))
+        ];
+
+        console.table(tableData);
+    } catch {
+        logger.printOperationError();
+    }
+}
+
+export async function createDirectory(directoryName) {
+    try {
+        const newPath = resolve(cwd(), directoryName);
+        await mkdir(newPath);
+    } catch (error) {
+        logger.printOperationError();
+    }
+}
